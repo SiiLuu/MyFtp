d1701e734bb07f9e4307c9449674f5d57f5e5d1e
lucas simao <lucas.simao@epitech.eu>
2020-04-14 19:37:26 +0200

    [MODIF] dele cmd

--- src/dele.c
+++ src/dele.c
@@ -7,29 +7,13 @@
 
 #include "ftp.h"
 
-void user_dele(server_t *server, int client, int id)
+bool check_file(int client, char *str, char *op)
 {
-    (void)id;
     DIR *mydir;
     struct dirent *myfile;
     struct stat mystat;
     char *buf = malloc(256);
-    bool found = false;
-    char *str = NULL;
-    char *op = NULL;
-    int i = 0;
 
-    op = strdup(server->clients[id].real_path);
-    str = strdup(server->command);
-    str[strlen(str)-2] = 0;
-    strcat(op, "/");
-    strcat(op, (str + 5));
-    i = (strlen(rindex(op, '/')));
-    op[strlen(op) - i] = 0;
-    if (strchr(str, '/') == NULL)
-        str = (rindex(str, ' ') + 1);
-    else
-        str = (rindex(str, '/') + 1);
     mydir = opendir(op);
     while ((myfile = readdir(mydir)) != NULL) {
         stat(buf, &mystat);
@@ -38,11 +22,33 @@ void user_dele(server_t *server, int client, int id)
             strcat(op, str);
             remove(op);
             dprintf(client, "250 Requested file action okay, completed.\r\n");
-            found = true;
-            break;
+            closedir(mydir);
+            free(buf);
+            return (true);
         }
     }
     closedir(mydir);
     free(buf);
+    return (false);
+}
+
+void user_dele(server_t *server, int client, int id)
+{
+    bool found = false;
+    char *str = NULL;
+    char *op = NULL;
+
+    op = strdup(server->clients[id].real_path);
+    str = strdup(server->command);
+    str[strlen(str)-2] = 0;
+    strcat(op, "/");
+    strcat(op, (str + 5));
+    op[strlen(op) - (strlen(rindex(op, '/')))] = 0;
+    if (strchr(str, '/') == NULL)
+        str = (rindex(str, ' ') + 1);
+    else
+        str = (rindex(str, '/') + 1);
+    if (check_file(client, str, op) == true)
+        found = true;
     (found == false) ? (command_not_found(server, client)) : (0);
 }
