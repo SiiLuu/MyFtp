09fa41b220520f5fd657a60bb682049f835ceb0b
lucas simao <lucas.simao@epitech.eu>
2020-04-15 14:39:36 +0200

    [MODIF] pwd.

--- src/commands.c
+++ src/commands.c
@@ -22,7 +22,7 @@ void user_pass(server_t *server, int client, int id)
 void user_pwd(server_t *server, int client, int id)
 {
     if (server->clients[id].log == true && server->clients[id].pass == true)
-        dprintf(client, "257 %s created.\r\n", server->clients[id].path);
+        dprintf(client, "257 \"%s\" created.\r\n", server->clients[id].path);
     else
         dprintf(client, "530 not logged in.\r\n");
 }
